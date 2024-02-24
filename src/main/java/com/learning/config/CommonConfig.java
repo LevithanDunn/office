5fd0df38b1dc04ffa0eb5ccebc8df4137ec264fe
dongjianwei <levithanmonster@163.com>
2024-02-24 18:41:29 +0800

    1.02 version

---
+++ src/main/java/com/learning/config/CommonConfig.java
@@ -0,0 +1,13 @@
+package com.learning.config;
+
+import org.springframework.context.annotation.Configuration;
+
+/**
+ * @author Jinawei
+ * @created 2024-02-24
+ */
+@Configuration
+public class CommonConfig {
+
+
+}
--- src/main/java/com/learning/controller/UserController.java
+++ src/main/java/com/learning/controller/UserController.java
@@ -1,11 +1,12 @@
 package com.learning.controller;
 
+import lombok.AllArgsConstructor;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 /**
  * <p>
- *  前端控制器
+ * 前端控制器
  * </p>
  *
  * @author Kim
@@ -13,6 +14,7 @@ import org.springframework.web.bind.annotation.RestController;
  */
 @RestController
 @RequestMapping("/user")
+@AllArgsConstructor
 public class UserController {
 
 }
